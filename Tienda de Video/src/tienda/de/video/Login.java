24c554e288c4662281ac9c106400a99a15388c4b
Camilo Andres D'isidoro Flechas <camilodisidoro2015@gmail.com>
2018-09-24 17:32:40 -0500

    se me olvido que hice aca :v

--- Tienda de Video/src/tienda/de/video/Login.java
+++ Tienda de Video/src/tienda/de/video/Login.java
@@ -10,11 +10,11 @@ import javax.swing.JOptionPane;
  *
  * @author Camilo D'isidoro
  */
-public class Ventana extends javax.swing.JFrame {
+public class Login extends javax.swing.JFrame {
     /**
      * Creates new form Ventana_Prueba
      */
-    public Ventana() {
+    public Login() {
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -157,21 +157,23 @@ public class Ventana extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Ventana().setVisible(true);
+                new Login().setVisible(true);
             }
         });
     }
--- Tienda de Video/src/tienda/de/video/TiendaDeVideo.java
+++ Tienda de Video/src/tienda/de/video/TiendaDeVideo.java
@@ -1,8 +1,7 @@
 package tienda.de.video;
-import tienda.de.video.Ventana;
 
 public class TiendaDeVideo {
     public static void main(String[] args) {
-        new Ventana().setVisible(true);
+        new Login().setVisible(true);
     }
 }
